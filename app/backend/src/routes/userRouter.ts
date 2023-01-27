b7ed88fea5b65177e71f23cae093fef4b5bc02f3
Ivanzmartins <ivanzmartins@gmail.com>
2023-01-27 19:01:10 -0300

    Requisitos 5 e 7

--- app/backend/src/routes/userRouter.ts
+++ app/backend/src/routes/userRouter.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
+import verifyLogin from '../middlewares/loginVerify';
 import UserController from '../controllers/userController';
 
 const router = Router();
 
-router.post('/', UserController.login);
+router.post('/', verifyLogin, UserController.login);
 
 export default router;
