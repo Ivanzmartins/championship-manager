4aa00ead040656c4a53152aac518e430046e8b59
Ivanzmartins <ivanzmartins@gmail.com>
2023-01-31 18:46:04 -0300

    Requisito 27

--- app/backend/src/auth/jwt.ts
+++ app/backend/src/auth/jwt.ts
@@ -1,6 +1,6 @@
-import { SignOptions } from 'jsonwebtoken';
-import * as jwt from 'jsonwebtoken';
+import { SignOptions, verify, sign } from 'jsonwebtoken';
 import * as dotenv from 'dotenv';
+import { NextFunction, Request, Response } from 'express';
 
 dotenv.config();
 
@@ -18,14 +18,27 @@ const jwtConfig: SignOptions = {
   algorithm: 'HS256',
 };
 
-export const createToken = (payload: TypeToken) => jwt.sign(payload, secret, jwtConfig);
+export const createToken = (payload: TypeToken) => sign(payload, secret, jwtConfig);
 
 export const validateToken = (token: string) => {
   try {
-    const decoded = jwt.verify(token, secret);
+    const decoded = verify(token, secret);
 
     return decoded;
   } catch (err) {
     return null;
+  }
+};
+
+export const tokenMiddleware = (req: Request, res: Response, next: NextFunction) => {
+  const { authorization } = req.headers;
+  if (!authorization) {
+    return res.status(401).json({ message: 'Token not found' });
+  }
+  try {
+    verify(authorization, secret) as TypeToken;
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: 'Token must be a valid token' });
   }
 };
--- app/backend/src/routes/matchesRouter.ts
+++ app/backend/src/routes/matchesRouter.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
+import { tokenMiddleware } from '../auth/jwt';
 import MatchesController from '../controllers/matchesController';
 
 const matchesRouter = Router();
 
 matchesRouter.get('/', MatchesController.getMatches);
 
-matchesRouter.post('/', MatchesController.createMatch);
+matchesRouter.post('/', tokenMiddleware, MatchesController.createMatch);
 
 matchesRouter.patch('/:id/finish', MatchesController.updateMatch);
 
