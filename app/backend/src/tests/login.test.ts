f8b7c21c21061a1f853351326e074c253e2a5a39
Ivanzmartins <ivanzmartins@gmail.com>
2023-02-02 17:41:49 -0300

    mais testes

---
+++ app/backend/src/tests/login.test.ts
@@ -0,0 +1,41 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import { app } from '../app';
+
+// import { Response } from 'superagent';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe("Testes da rota /login", () => { 
+  describe('POST /login', () => {
+    it('Login com as credenciais corretas deve retornar um token com status 200', async () => {
+      const httpRes = await chai
+        .request(app)
+        .post('/login')
+        .send({
+          email: "admin@admin.com",
+          password: "secret_admin"
+        });
+    expect(httpRes.status).to.be.equal(200);
+    expect(httpRes.body).to.have.property('token');
+    });
+})
+  describe('Testa a rota /login/validate', () => {
+    it('Verifica se o token da pessoa é válido', async () => {
+      const token = "fghfgh";
+      const httpRes = await chai
+      .request(app)
+      .get('/login/validate')
+      .set('Authorization', `Bearer ${token}`);
+      console.log(httpRes.body);
+      
+      // expect(httpRes.status).to.be.equal(200);
+      // expect(httpRes.body).to.have.property('role');
+    })
+  });
+});
